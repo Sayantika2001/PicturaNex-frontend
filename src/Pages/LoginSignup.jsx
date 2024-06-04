06acad69b303957f4ea3fdd8c945b0fa91d51ca1
Sayantika Manna <mannasayantika204@gmail.com>
2024-06-04 19:25:13 +0530

    first commit

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ import './LoginSignup.css';
     const login = async () => {
         console.log("Login Function Executed",formData);
         let responseData;
-        await fetch('http://localhost:4000/login',{
+        await fetch('https://picturanex-backend.onrender.com/login',{
             method: 'POST',
             headers:{
                 Accept:'application/form-data',
@@ -37,7 +37,7 @@ import './LoginSignup.css';
     const signup = async () => {
         console.log("Signup Function Executed",formData);
         let responseData;
-        await fetch('http://localhost:4000/signup',{
+        await fetch('https://picturanex-backend.onrender.com/signup',{
             method: 'POST',
             headers:{
                 Accept:'application/form-data',
