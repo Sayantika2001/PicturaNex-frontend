9f5e79050a98d0268e1e26b20de0621b6471204d
Sayantika Manna <mannasayantika204@gmail.com>
2024-06-04 20:03:54 +0530

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,40 @@
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import './Navbar.css'
-import { Link } from 'react-router-dom'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/Download1.png'
 
 const Navbar = () => {
-
   const [menu, setMenu] = useState("Home");
+  const Navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('auth-token');
+    Navigate('/');
+  };
 
   return (
     <div className='navbar'>
-    <div className="nav-logo">
-        <img src={logo} width={150} height={100} alt=''/>
+      <div className="nav-logo">
+        <img src={logo} width={150} height={100} alt='PicturaNex Logo' />
         <p>PicturaNex</p>
-    </div>
-    <ul className='nav-menu'>
-    <Link to = '/'><li onClick={()=>{setMenu("Home")}}>Home</li></Link>
-    <Link to = '/About'><li onClick={()=>{setMenu("About")}}>About Us</li></Link>
-    <Link to = '/Contact'><li onClick={()=>{setMenu("Contact")}}>Contact Us</li></Link>
-    </ul>
-    <div className='nav-login-cart'>
-        <Link to='/Cart'><img src={cart_icon} alt=''/></Link>
+      </div>
+      <ul className='nav-menu'>
+        <Link to='/'><li className={menu === "Home" ? "active" : ""} onClick={() => setMenu("Home")}>Home</li></Link>
+        <Link to='/About'><li className={menu === "About" ? "active" : ""} onClick={() => setMenu("About")}>About Us</li></Link>
+        <Link to='/Contact'><li className={menu === "Contact" ? "active" : ""} onClick={() => setMenu("Contact")}>Contact Us</li></Link>
+      </ul>
+      <div className='nav-login-cart'>
+        <Link to='/Cart'>
+          <img src={cart_icon} alt='Cart Icon' />
+          <div className='nav-cart-count'></div> {/* Example cart count */}
+        </Link>
         {localStorage.getItem('auth-token')
-        ?<button onClick={()=>{localStorage.removeItem('auth-token'); window.location.replace('/')}}>Logout</button>
-      : <Link to='/LoginSignup'><button>Login</button></Link>}
-        </div>
+          ? <button onClick={handleLogout}>Logout</button>
+          : <Link to='/LoginSignup'><button>Login</button></Link>}
+      </div>
     </div>
   )
 }
-export default Navbar
+
+export default Navbar;
